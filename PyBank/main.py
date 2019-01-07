e8085b400a9c58d37fada2861273d25f702c63e4
RR <reemaratnani@yahoo.ca>
2019-01-07 00:49:51 -0800

    updated script

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,6 +10,13 @@ with open('budget_data.csv','r') as csvfile:
     print(Total)
     Monthly_change = [(x[1]-x[0])for x in zip(new_list[:-1],new_list[1:])]
     Average_change = sum(Monthly_change)/len(Monthly_change)
-    print(Average_change)
-    print(max(Monthly_change))
-    print(min(Monthly_change))
+    print(round(Average_change, 2))
+    csvfile.seek(0)
+    next(csvfile)
+    next(csvfile)
+    month_column = [r[0] for r in csv_reader]
+    Month_Monthchange = dict(zip(month_column,Monthly_change))
+    Greatest_increase = max(zip(Month_Monthchange.values(), Month_Monthchange.keys()))
+    Greatest_decrease = min(zip(Month_Monthchange.values(), Month_Monthchange.keys()))
+    print(Greatest_increase)
+    print(Greatest_decrease)
